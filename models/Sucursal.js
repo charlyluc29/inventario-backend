f518d77682ab99731b47d389fe0a4687672f775d
charlyluc29 <charlyluc29@gmail.com>
2026-01-18 22:44:47 -0600

    mensaje

--- models/Sucursal.js
+++ models/Sucursal.js
@@ -1,13 +1,8 @@
 const mongoose = require("mongoose");
 
-const sucursalSchema = new mongoose.Schema(
-  {
-    nombre: { type: String, required: true },
-    direccion: String
-  },
-  {
-    collection: "sucursales" // 👈 NOMBRE REAL EN ATLAS
-  }
-);
+const sucursalSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  direccion: String
+});
 
 module.exports = mongoose.model("Sucursal", sucursalSchema);
