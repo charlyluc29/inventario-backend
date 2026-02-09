08aca120a506694186039f70e5ec7b4a043f262b
charlyluc29 <charlyluc29@gmail.com>
2026-02-09 00:16:19 -0600

    mensaje

--- models/Inventario.js
+++ models/Inventario.js
@@ -1,38 +1,26 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const inventarioSchema = new mongoose.Schema(
   {
-    producto: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Producto",
-      required: true
+    producto: { 
+      type: mongoose.Schema.Types.ObjectId, // <-- ObjectId
+      ref: "Producto", 
+      required: true 
     },
-
-    sucursal: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Sucursal",
-      required: true
+    sucursal: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "Sucursal", 
+      required: true 
     },
-
-    cantidad: {
-      type: Number,
-      required: true,
-      min: 0
-    },
-
-    // 🔧 NUEVO
-    estadoReparacion: {
-      type: String,
-      enum: ["pendiente", "reparado"],
-      default: "pendiente"
+    cantidad: { 
+      type: Number, 
+      required: true, 
+      min: [0, "La cantidad no puede ser negativa"] 
     }
   },
   { timestamps: true }
-)
+);
 
-inventarioSchema.index(
-  { producto: 1, sucursal: 1 },
-  { unique: true }
-)
+inventarioSchema.index({ producto: 1, sucursal: 1 }, { unique: true });
 
-module.exports = mongoose.model("Inventario", inventarioSchema)
+module.exports = mongoose.model("Inventario", inventarioSchema);
